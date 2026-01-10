95e3f295036bfa29fb1d1159fc5b08d3f87d05a0
aishwarya18bh <aishwaryabh18@gmail.com>
2026-01-10 17:24:49 +0530

    CORS issue fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,9 +29,18 @@ const authLimiter = rateLimit({
 });
 
 // CORS
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      "http://localhost:3000",
+      "https://subscription-tracker.vercel.app",
+    ],
     credentials: true,
   })
 );
